f433878aacdbb5ff82130b59e70a11e60dd4b417
Nazarova Diana <Dnazarova@korusconsulting.ru>
2025-03-05 14:20:53 +0300

    added overlay to the recipe card

--- src/components/RecipeCard.vue
+++ src/components/RecipeCard.vue
@@ -1,6 +1,20 @@
 <template>
   <div class="recipe-card">
-    <img alt="recipe-card" src="../assets/img/recipe-card.png" />
+    <div class="image-container">
+      <img alt="recipe-card" src="../assets/img/recipe-card.png" />
+      <div class="overlay">
+        <div class="time">1ч 17м</div>
+        <div class="ingredients">
+          <p>ингредиенты:</p>
+          <ul>
+            <li>огурцы</li>
+            <li>болгарский перец</li>
+            <li>салат айсберг</li>
+            <li>зеленый лук</li>
+          </ul>
+        </div>
+      </div>
+    </div>
     <div class="description flex">
       <h3>Постный салат</h3>
       <SvgIcon class="like-fill" name="like-fill" />
@@ -20,6 +34,54 @@ export default {
 <style lang="scss" scoped>
 .recipe-card {
   background-color: #e3cbbc;
+  border-radius: 6px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.15);
+  cursor: pointer;
+
+  .image-container {
+    position: relative;
+
+    img {
+      border-top-left-radius: 6px;
+      border-top-right-radius: 6px;
+    }
+  }
+
+  .overlay {
+    position: absolute;
+    top: 0;
+    left: 0;
+    padding: 19px;
+    box-sizing: border-box;
+    border-top-left-radius: 6px;
+    border-top-right-radius: 6px;
+    width: 100%;
+    height: 100%;
+    background: rgba(47, 34, 34, 0.65);
+    opacity: 0;
+    color: #fff;
+    transition: opacity 0.3s ease-in-out;
+
+    .time {
+      text-align: end;
+      font-size: 16px;
+      margin-bottom: 30px;
+    }
+
+    .ingredients {
+      p {
+        text-transform: uppercase;
+      }
+
+      ul {
+        list-style: none;
+      }
+    }
+  }
+
+  &:hover .overlay {
+    opacity: 1;
+  }
 
   .description {
     padding: 15px 20px 20px;
