11341e76177637db9cd932815747f1988b1e860e
EVasilevskaya <EVasilevskaya@mons.ru>
2025-04-29 22:10:58 +0300

    not-found page added

---
+++ src/views/NotFound.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="layout flex">
+    <div class="not-found">
+      <span class="title">
+        404
+      </span>
+      <span>Страница не найдена</span>
+      <p class="back-button" @click="goBack">Назад</p>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+const goBack = () => {
+  router.back()
+}
+</script>
+
+<style lang="scss" scoped>
+.not-found {
+  display: flex;
+  flex-direction: column;
+  gap: 1.5rem;
+  align-items: center;
+  justify-content: center;
+
+  font-size: 30px;
+
+}
+
+.title {
+  font-size: 180px;
+}
+
+.back-button {
+  border-bottom: 1px solid white;
+  color: white;
+  cursor: pointer;
+  font-size: 30px;
+
+}
+</style>
